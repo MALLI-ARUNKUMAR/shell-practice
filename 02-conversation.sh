d000d9e8c81e33b8f17c63fe9c32e7e1b75b315a
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-15 19:51:21 +0530

    changed something

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-PERSON1=suresh
-PERSON2=ramesh
 
-echo "$PERSON1 : : hello  $PERSON2, How are you?"
-echo "$PERSON2 : : hi $PERSON1 i am fine thanks for asking .How are you?"
-echo "$PERSON1: : doing fine.What are you learning $PERSON2"
-echo "$PERSON2 : : I am learning shell,What abou you?" 
+echo "suresh : : hello  ramesh, How are you?"
+echo "ramesh : : hi suresh i am fine thanks for asking .How are you?"
+echo "suresh : : doing fine.What are you learning ramesh"
+echo "ramesh : : I am learning shell,What abou you?" 
---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 : : hello  $PERSON2, How are you?"
+echo "$PERSON2 : : hi $PERSON1 i am fine thanks for asking .How are you?"
+echo "$PERSON1: : doing fine.What are you learning $PERSON2"
+echo "$PERSON2 : : I am learning shell,What abou you?" 
