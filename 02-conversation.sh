6aab2049795264558806390ecd80c16838aa1d9d
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-15 19:38:20 +0530

    changed something

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "suresh : : hello ramesh , How are you?"
 echo "ramesh : : hi suresh i am fine thanks for asking .How are you?"
 echo "suresh : : doing fine.What are you learning ramesh"
