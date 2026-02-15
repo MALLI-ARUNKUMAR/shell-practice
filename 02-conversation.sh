32068b2d5ce1202639da29281c40225c3debadb6
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-15 19:30:32 +0530

    changed something

---
+++ 02-conversation.sh
@@ -0,0 +1,4 @@
+echo "suresh : : hello ramesh , How are you?"
+echo "ramesh : : hi suresh i am fine thanks for asking .How are you?"
+echo "suresh : : doing fine.What are you learning ramesh"
+echo "ramesh : : I am learning shell,What abou you?" 
