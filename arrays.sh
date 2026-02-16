f9c72693e8180b15ed301f000441bac944f054e5
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-16 19:13:23 +0530

    changed somethong

--- arrays.sh
+++ arrays.sh
@@ -3,6 +3,6 @@
 #arrays#
 
 FRUITS={apple,banana,mango}
-echo "first fruit is:${fruits[0]}"
+echo "first fruit is:{$fruits[0]}"
 echo "second fruit is:${fruits[1]}"
 echo "third fruit is:${fruits[2]}"
