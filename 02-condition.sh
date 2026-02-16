d8ac7db17e7d593316348fd1c2e49fa126507b9d
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-16 19:53:05 +0530

    changed somethong

--- 02-condition.sh
+++ 02-condition.sh
@@ -2,7 +2,7 @@
 
 NUMBER=25
 if [ $NUMBER -gt 20 ]; then
-echo"given number:$NUMBER is greater than 20"
+echo "given number:$NUMBER is greater than 20"
 else
-echo "given number:$NUMBER is lessthan than 20"
+echo "given number: $NUMBER is lessthan than 20"
 fi
