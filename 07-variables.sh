711129cd4b843594642116f667c3c51cb090125c
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-16 18:48:52 +0530

    changed somethong

--- 07-variables.sh
+++ 07-variables.sh
@@ -5,7 +5,7 @@
 echo " all args passed to the script : $@"
 echo "number of variables passed to the script :$#"
 echo "script name : $0"
-echo "present directory :$pwd"
+echo "present directory :$PWD"
 echo "who is running : $USER"
 echo "home directory of current user :$HOME"
 echo "PID of the script :$$"
