fbbddd48447f1bef4c3e25417ef0349cc1161f2c
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-16 18:47:15 +0530

    changed somethong

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,8 +6,8 @@ echo " all args passed to the script : $@"
 echo "number of variables passed to the script :$#"
 echo "script name : $0"
 echo "present directory :$pwd"
-echo "who is running : $user"
-echo "home directory of current user :$home"
+echo "who is running : $USER"
+echo "home directory of current user :$HOME"
 echo "PID of the script :$$"
-sleep 100 &
-echo "PID of recently executed background process :&!"
+sleep 100&
+echo "PID of recently executed background process :$!"
