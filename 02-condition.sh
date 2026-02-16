f40736b80cbb630ab6bfb8c2603c79e061dc5050
malli arunkumar <malliarunkumar777@gmail.com>
2026-02-16 20:03:40 +0530

    changed somethong

--- 02-condition.sh
+++ 02-condition.sh
@@ -6,12 +6,12 @@ if [ $NUMBER -gt 20 ]; then
 
 echo "given number:$NUMBER is greater than 20"
 
-elif [ $NUMBER -eq 20]; then
+elif [ $NUMBER -eq 20 ]; then
 
-echo "given number:$NUMBER is greater than 20"
+echo "given number:$NUMBER is equal to 20"
 
 else 
 
-echo "given number: $NUMBER is lessthan than 20 "
+echo "given number: $NUMBER is less than 20 "
 
 fi
