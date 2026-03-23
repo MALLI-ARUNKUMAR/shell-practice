d702e89829cc938fcaa5454a000aa355d5826cb1
malli arunkumar <malliarunkumar777@gmail.com>
2026-03-23 05:51:30 +0530

    changed something

---
+++ 04-conversation.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "PERSON1 : : hello  PERSON2, How are you?"
+echo "PERSON2 : : hi PERSON1 i am fine thanks for asking .How are you?"
+echo "PERSON1 : : doing fine.What are you learning PERSON2"
+echo "PERSON2 : : I am learning shell,What abou you?"
