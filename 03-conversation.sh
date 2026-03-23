44c76c0bb50951171301be4dc308fbfe5ef4ef43
malli arunkumar <malliarunkumar777@gmail.com>
2026-03-23 05:45:52 +0530

    changed something

---
+++ 03-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON 1=suresh
+PERSON 2=ramesh
+
+echo "PERSON 1 : : hello  PERSON 2, How are you?"
+echo "PERSON 2 : : hi PERSON 1 i am fine thanks for asking .How are you?"
+echo "PERSON 1 : : doing fine.What are you learning PERSON 2"
+echo "PERSON 2 : : I am learning shell,What abou you?"
