0f60e503e43ec853c8bd3fb32d60e411b656087a
malli arunkumar <malliarunkumar777@gmail.com>
2026-03-24 11:51:59 +0530

    changed something

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,6 +4,10 @@ number=$1
 
 if [ $number -gt 20 ]; then
 
-echo "$number is greater then 20"
+echo "Given number :$number is greater then 20"
+
+else [ $number -lt 20 ]; then
+
+echo "Given number :$number is lessthan  20"
 
 fi
