7c6e6bb06bb1854a224c66b18d04817abab98f5d
malli arunkumar <malliarunkumar777@gmail.com>
2026-03-25 12:24:19 +0530

    changed something

--- 16-colors.sh
+++ 16-colors.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
 
-echo  -e " $R hello world "
+echo  -e " $R hello world $N $G my name is $N $Y arunkumar $N $B i am ggod boy $n "
