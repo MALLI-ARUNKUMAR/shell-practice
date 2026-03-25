77f7a096cb7e5b91a7f10e9e7344f987825ffe4c
malli arunkumar <malliarunkumar777@gmail.com>
2026-03-25 09:02:08 +0530

    changed something

---
+++ 11-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [USER_ID -ne 0 ]; then
+
+echo "please run the script with root access "
+exit 1
+fi
+
+VALIDATE () {
+    if [$1 -ne 0]
+        echo "$2 is.......failure"
+    else
+        echo "$2 is......success"
+}
+
+dnf install nginx -y
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "installing mysql"
+
+dnf install nodejs -
+VALIDATE $? "installing nodejs"
