c5dbf6556b69947ba0441da8dc1be6a516dc7341
malli arunkumar <malliarunkumar777@gmail.com>
2026-03-25 11:40:05 +0530

    changed something

--- 15-loops.sh
+++ 15-loops.sh
@@ -12,7 +12,7 @@ fi
 mkdir -p $logsfolder
 
 VALIDATE () {
-    if [$1 -ne 0 ]; then
+    if [ $1 -ne 0 ]; then
         echo "$2 is.......failure" | tee -a $logsfile
      else
         echo "$2 is......success" | tee -a $logsfile
